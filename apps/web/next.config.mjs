c91257c0c7ee93126cd4d25fcc1b675304ca0944
Blanca <blayudoco@gmail.com>
2026-06-15 14:17:45 +0200

    permitiendo a links verse

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -31,10 +31,11 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'ktwnpnzopnucgfpmleuk.supabase.co',
+        hostname: 'hwgudaptfhyeotuomhcq.supabase.co',
       },
     ],
   },
 }
+
 
 export default withPWA(nextConfig)
